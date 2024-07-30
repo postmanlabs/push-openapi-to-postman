db0ca2f1e01ffb151045f01eb53cea08d5cef596
Alexander Rubia <alexander.rubia@postman.com>
2024-07-30 18:06:42 +0200

    [PAPI-2823] feat: Allow YAML files

--- readFile.js
+++ readFile.js
@@ -1,12 +1,24 @@
 const core = require('@actions/core');
 const { promises: fs } = require('fs');
-
+const yaml = require('yaml');
 
 const readFile = async (path) => {
+
     core.info(`Reading ${path} file ...`);
     let content = await fs.readFile(path, 'utf8');
-    core.debug(`File read`);
-    return JSON.stringify(JSON.parse(content.toString()), null, 2);
+
+    try {
+        core.debug(`File read`);
+        return JSON.stringify(JSON.parse(content.toString()), null, 2);
+    } catch (e) {
+        try {
+            const [doc] = new yaml.Parser().parse(content.toString())
+            if (doc.value.items && doc.value.items.length > 0) {
+                return yaml.stringify(yaml.parse(content.toString()));
+            }
+        } catch (ye) { /* empty */ }
+        throw e;
+    }
 }
 
 module.exports = readFile;
